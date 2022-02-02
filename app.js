f87c9a54e690caa7ce240ad5dcb348afe7525117
Roshan Paudel <rozanpoudel@gmail.com>
2022-02-02 11:16:51 +0545

    home routing push

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello from ejs");
+});
+
 app.listen(port, () => {
   console.log(`listening to Port:${port}`);
 });
