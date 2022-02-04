7e6a471750a5f28f36bf4d718a0d25e67f0f8066
Roshan Paudel <rozanpoudel@gmail.com>
2022-02-04 18:23:59 +0545

    Yelpcamp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 const Campground = require("./models/campground");
 mongoose.connect("mongodb://127.0.0.1:27017/campground");
@@ -15,6 +16,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
